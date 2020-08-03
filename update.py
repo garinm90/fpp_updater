2a8b2e9dea5d65513d66e328c489532f0443274f
Falcon Player <fpp@FPP>
2020-08-03 17:24:20 +0100

    Setup file and logging init commit

---
+++ update.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python3
+import logging
+
+logging.basicConfig(filename="/mnt/update/logs/update.log",
+                    level=logging.DEBUG)
+logging.debug("Module and Updater ran!!")
