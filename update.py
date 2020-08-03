ec65cfd8067eb7f131e4630f9983a81b86316584
Max Garin <maxgarin@gmail.com>
2020-08-03 20:13:13 +0100

    Compare online status vs what should be online

--- update.py
+++ update.py
@@ -4,6 +4,7 @@ import multiprocessing
 import subprocess
 import os
 import requests
+import json
 
 logging.basicConfig(filename="/mnt/update/logs/update.log",
                     level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%b-%d-%y %I:%M %p')
@@ -13,69 +14,41 @@ medallion_pi_ip = "192.168.1.112"
 ticket_booth_pi_ip = "192.168.1.113"
 wheel_pi_ip = "192.168.1.111"
 
+
 ip_addresses = [medallion_pi_ip, ticket_booth_pi_ip, wheel_pi_ip]
 
 
-def pinger(job_q, results_q):
-    DEVNULL = open(os.devnull, 'w')
-    while True:
-        ip = job_q.get()
-        if ip is None:
-            break
-
-        try:
-            subprocess.check_call(['ping', '-c1', ip],
-                                  stdout=DEVNULL)
-            results_q.put(ip)
-        except:
-            pass
-
-# Reenable when done with other functions as this slows down the script.s
-# pool_size = len(ip_addresses)
-
-# jobs = multiprocessing.Queue()
-# results = multiprocessing.Queue()
-
-# pool = [multiprocessing.Process(target=pinger, args=(jobs, results))
-#         for i in range(pool_size)]
-
-# for p in pool:
-#     p.start()
-
-# for ip in ip_addresses:
-#     jobs.put(ip)
-
-# for p in pool:
-#     jobs.put(None)
-
-# for p in pool:
-#     p.join()
-
-# ips = []
-# while not results.empty():
-#     ips.append(results.get())
-# print(ips)
-
-
 def get_json(ip_address, api_url):
-    json = requests.get(f"http://{ip_address}/api/" + api_url)
+    json = requests.get(f"http://{ip_address}/api/" + api_url).json()
     return json
 
 
 def compare_sequences(origin, *targets):
-    origin_sequences = get_json(origin, "sequence").json()
+    origin_sequences = get_json(origin, "sequence")
     for target in targets:
-        target_sequences = get_json(target, "sequence").json()
+        target_sequences = get_json(target, "sequence")
         if target_sequences == origin_sequences:
-            print("success")
+            pass
         else:
             print(f"{target} is missing sequences!")
             upload = requests.get(
                 f"http://{origin}/copyFilesToRemote.php?ip={target}")
             print(upload.text)
 
-        # if get_json(medallion_pi_ip, "sequence").json() == get_json("192.168.1.91", "sequence").json():
-        #     print("They're same")
+
+# compare_sequences("192.168.1.91", "192.168.1.90")
+
+def get_online_pis():
+    systems = get_json(medallion_pi_ip, "fppd/multiSyncSystems")["systems"]
+    ips = []
+    for system in systems:
+        ips.append(system["address"])
+    return ips
 
 
-compare_sequences("192.168.1.91", "192.168.1.90")
+def check_pi_status():
+    online_pis = get_online_pis()
+    online_pis = set(online_pis)
+    ip_addresses = set(ip_addresses)
+    logging.error(
+        msg=f"The following are offline {ip_addresses.difference(online_pis)}")
